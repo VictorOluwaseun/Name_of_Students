92bc23269791a6f64567ff797dd546657f8047f7
Victor A <VictorOluwaseun@Macs-MacBook-Air.local>
2020-08-19 17:07:38 +0100

    Finished RecyclerView implementation

--- app/src/main/java/com/decagon/listofstudents/MainActivity.kt
+++ app/src/main/java/com/decagon/listofstudents/MainActivity.kt
@@ -2,6 +2,9 @@ package com.decagon.listofstudents
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,6 +31,7 @@ class MainActivity : AppCompatActivity() {
         )
 
         val adapter = StudentAdapter(studentNames)
-
+        rvStudents.adapter = adapter
+        rvStudents.layoutManager = LinearLayoutManager(this)
     }
 }
